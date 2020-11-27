fb3419525669abb1dfc12dc925303426f8d93ccf
Josh White <whitejoshua999@gmail.com>
2020-11-27 14:46:12 +0000

    to do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
 
+  state={
+    inputText: "",
+    list: [],
+    delete: this.delete.bind(this),
+  }
+  
+  handleInput = (event) => {
+    this.setState({inputText: event.target.value})
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      list: [...this.state.list, this.state.inputText],
+      inputText: "",
+    })
+    console.log("submit")
+  }
+  delete(id){
+    this.setState(prevState => ({
+      list: prevState.list.filter(item => item != id)
+    }))
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleInput}
+            type="text"
+            value={this.state.inputText}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+
+        <div>
+          {this.state.list.map((item) => {
+            return <li onClick={this.delete.bind(this, item)}>{item}</li>
+          })}
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
